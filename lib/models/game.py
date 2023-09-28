a82330469fb7ea58ef3b8e913eb17bdf161b8b98
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-09-28 12:32:05 -0600

    create seed framework

--- lib/models/game.py
+++ lib/models/game.py
@@ -16,3 +16,5 @@ class Game:
         """
         CURSOR.execute(sql)
         CONN.commit()
+        
+    
---
+++ lib/seed.py
@@ -0,0 +1,18 @@
+from models.__init__ import CONN, CURSOR
+from models.game import Game
+from models.review import Review
+from faker import Faker
+
+fake = Faker()
+
+
+# drop tables each time the files runs
+# create the tables
+# write a loop to create 10 games
+# 3 reviews for each game
+
+
+
+def seed_database():
+    pass
+    
