4b7b63ef58c67875c934e26e6248e07cf56368f9
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-10-03 13:43:03 -0600

    add properties for Review and plan CLI menu and logic

--- PLANNING.md
+++ PLANNING.md
@@ -10,3 +10,32 @@ Once a day for 15 min
 
 9/28 - Next step create/save/drop_table class methods for model classes then go complete faker seeding in seed.py
 10/2 - Next step properties, getter/setter and class constraints
+10/3 - Update menu in cli.py to reflect below logic and build ORM methods to support menu/DB functionality
+
+
+
+
+MENU:
+
+A user can:
+- browse
+    - list reviews based on genre
+        - enter genre
+    - view games based on genre
+        - view reviews for a game
+    - view games in alpha order
+        - view reviews for a game
+    - Views games based on average ratings 
+        - view reviews for a game
+
+- manage 
+    - view games in alpha order (user selects a number) or go up menu
+        - enter a new game
+        - update a game
+        - enter a new review for and existing game (X) 
+        - display reviews for chosen game
+            - select review and update it
+            - select review and delete it       
+   
+
+To enter a review, a menu should display all games and their corresponding IDs. The selection is captured from the user input and stored in the review.game_id instance
--- lib/models/review.py
+++ lib/models/review.py
@@ -12,6 +12,51 @@ class Review:
         self.content = content
         self.game_id = game_id
         
+    @property
+    def header(self):
+        return self.__header
+    
+    @header.setter
+    def header(self, header):
+        if not isinstance(header, str):
+            raise TypeError("Header must be a string")
+        self.__header = header
+        
+    @property
+    def rating(self):
+        return self.__rating
+    
+    @rating.setter
+    def rating(self, rating):
+        if not isinstance(rating, int):
+            raise TypeError("Rating must be an integer")
+        if rating < 1 or rating > 10:
+            raise ValueError("Rating must be between 1 and 10")
+        self.__rating = rating
+    
+    @property
+    def content(self):
+        return self.__content
+    
+    @content.setter
+    def content(self, content):
+        if not isinstance(content, str) or not len(content) >= 10:
+            raise TypeError("Content must be a string and at least 10 characters")
+        self.__content = content
+        
+    @property
+    def game_id(self):
+        return self.__game_id
+    
+    @game_id.setter
+    def game_id(self, game_id):
+        if not isinstance(game_id, int):
+            raise TypeError("Game ID must be an integer")
+        if game_id not in Game.find_by_id(game_id):
+            raise ValueError("Game ID does not exist")
+        self.__game_id = game_id
+    
+        
     def save(self):
         """ Persist the attributes of a Review instance to the database """
         sql = """
